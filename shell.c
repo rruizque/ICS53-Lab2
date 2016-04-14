8550cdc90d5661a0292f3477e747cb37cd61c816
maxpaulus43 <maxpaulus43@gmail.com>
2016-04-14 14:16:29 -0700

    changed main loop

--- shell.c
+++ shell.c
@@ -8,36 +8,37 @@
 int main() {
 
 
-  // char command[20];
+  char command[20];
 
-  // do {
-  //   printf("\nprompt> ");
-  //   scanf("%s", command);
-
+  do {
+    printf("prompt> ");
+    scanf("%s", command);
 
     
-  // } while (!strcmp("quit", command) == 0);
-    int status;
-    int pid;
-    pid=fork();
-    if(pid<0)
-    {
-        printf("\n Error ");
-        exit(1);
-    }
-    else if(pid==0)
-    {
-        printf("\n Hello I am the child process ");
-        printf("\n My pid is %d ",getpid());
-        exit(0);
-    }
-    else
-    {
-        wait(&status);
-        printf("\n Hello I am the parent process ");
-        printf("\n My actual pid is %d \n ",getpid());
-        exit(1);
-    }
+     
+  } while (!strcmp("quit", command) == 0);
+  
+  int status;
+  int pid;
+  pid=fork();
+  if(pid<0)
+  {
+      printf("\n Error ");
+      exit(1);
+  }
+  else if(pid==0)
+  {
+      printf("\n Hello I am the child process ");
+      printf("\n My pid is %d ",getpid());
+      exit(0);
+  }
+  else
+  {
+      wait(&status);
+      printf("\n Hello I am the parent process ");
+      printf("\n My actual pid is %d \n ",getpid());
+      exit(1);
+  }
 
   return 0;
 }
