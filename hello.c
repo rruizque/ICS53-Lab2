f07d634793075d7e72ed2cd28d440264b47d0cea
maxpaulus43 <maxpaulus43@gmail.com>
2016-04-15 14:18:38 -0700

    updated the hello function

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
-  sleep(2);
-	printf("Hi Max and Rodrigo! You guys ROCK!\n");
-  sleep(2);
+int main(int argc, char** argv) {
+  if (argc == 2) {
+    sleep(2);
+	  printf("Hi %s! You ROCK!\n", argv[1]);
+    sleep(2);
+  }
+  else {
+    printf("Hello: Not enough args\n");
+  }
 	return 0;
 }
